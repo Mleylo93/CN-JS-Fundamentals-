176207d46b3830534b4180380507a33e231ec33a
Marc Leyland <marc.leyland93@outlook.com>
2021-11-22 15:53:48 +0000

    Collection of code that meets the criteria

---
+++ criteria.js
@@ -0,0 +1,86 @@
+
+//Arrays and using them
+// let faveFilms =[
+//     "The Matrix",
+//     "Final Destination",
+//     "Friday the 13th",
+    
+//     ];
+    
+//     console.log(faveFilms);
+//     faveFilms.push("Halloween", "Midsommar");
+    
+//     console.log(faveFilms);
+    
+//     for(let i = 0; i<5; i++){
+//         console.log(faveFilms);
+//     }
+
+
+// Fizz Buzz  
+//   let num= 15
+// if (num % 3==0 && num % 5==0){
+//     console.log("Fizz Buzz")
+// }
+// else if (num % 3==0) {
+//     console.log("Fizz")
+// }
+// else if(num% 5==0){
+//     console.log("Buzz")
+// }
+// else {
+//     console.log(`${num} is not divisible by 3 or 5`)
+// }
+
+
+
+// Cash Machine Program
+// const cashWithdrawal= (amount, accnum) =>{
+//     console.log(`Withdrawing ${amount} from account ${accnum}`);
+
+// }
+// cashWithdrawal (300, 50449921);
+// cashWithdrawal(30, 50449921);
+// cashWithdrawal(200,50447921); 
+
+
+// const addUp = (num1, num2)=> {
+//     return num1 + num2;
+// }
+
+// let a = (addUp(1,3));
+// console.log(a);
+
+// Cash machine using if else statements
+// const cashMachine=(pin, amount) => {
+//     let balance = ("2002")
+//     const custPin= ("1172")
+//     let newBalance= (balance- amount);
+
+//     if ((pin== custPin) && (amount<= balance)){
+//     console.log(`Your pin is correct, you have withdrawn £${amount} your new balance is £${newBalance}`)
+       
+//     }
+
+//     else if (pin != custPin){
+//         console.log("Your pin is incorrect, please try again")
+//     }
+
+//     else{
+//         console.log("You have insufficient funds")
+//     }
+
+
+//If else statement
+// let age= "18"
+// let country= "UK"
+// if (age >=18 && country =="UK")
+// {
+//     console.log (`You are ${age} and from the UK so I can serve you`);
+
+// }
+
+// else{
+
+//     console.log("Sorry, you are younger than 18 so I am unable to serve you")
+// }
